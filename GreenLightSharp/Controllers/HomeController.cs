f113997c1bb2ed3a88935e05f16ce933a3555e54
Conner Martin <connersmartin@gmail.com>
2018-09-11 23:07:26 -0700

    added in AddMember logic

--- GreenLightSharp/Controllers/HomeController.cs
+++ GreenLightSharp/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using GreenLightSharp.Models;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
 
 namespace GreenLightSharp.Controllers
 {
@@ -24,6 +27,7 @@ namespace GreenLightSharp.Controllers
         public ActionResult AddBand(Show model)
         {
             //Create band with a given name
+
             //Go to AddMember view with bandId showing
             return View();
         }
@@ -38,6 +42,7 @@ namespace GreenLightSharp.Controllers
         public ActionResult AddMember(string bandID)
         {
             //Add Member page
+            //Need to figure out how to do this with partial views keeping the same front page essentially
             return View();
         }
 
@@ -45,6 +50,22 @@ namespace GreenLightSharp.Controllers
         public ActionResult AddMember(Member model)
         {
             //Add Member
+            //TODO Figure this out for adding a member
+            //Will need to know bandId for proper linkage can this be saved with a token?
+            //may need to implement a secondary table if these are 'universal/session' members, but that's definitely secondary
+
+            Member bandMember = new Member { Name = model.Name, Instrument = model.Instrument, Status = "0", BandId = model.BandId };
+
+
+            // /member?&name=***
+            //API call to add a new member
+            //Need to figure out way to link bands and members 'easily'
+
+            //SIDENOTE, can 1 totally put this in its own data acces layer somehow TODO Todo TODOOOO and 2 make a Request method
+
+            GoogleRest(bandMember, Method.POST, "Member");
+
+            //this restResponse.content may be the best way to link bands/members
             //Goes to show page
             return View();
         }
@@ -53,6 +74,20 @@ namespace GreenLightSharp.Controllers
         {
             //Where everything is displayed and updated
             return View();
+        }
+
+        public string GoogleRest(object obj, Method method, string resource)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("");
+            var json = JsonConvert.SerializeObject(obj);
+            var request = new RestRequest(method);
+            request.AddParameter("application/json; charset=utf-8", json, ParameterType.RequestBody);
+            request.RequestFormat = DataFormat.Json;
+            request.Resource = string.Format("{0}.json", resource);
+            IRestResponse restResponse = client.Execute(request);
+
+            return restResponse.Content;
         }
     }
 }
